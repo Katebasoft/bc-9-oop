df6afdd77a540f2c6c75a7bbd81d8e230aa76537
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-18 21:19:31 +0300

    Adds comments tostudent.py

--- student_class/student.py
+++ student_class/student.py
@@ -5,7 +5,7 @@ map_country = {
 	"UG": "Uganda",
 	"TZ": "Tanzania"
 }
-
+#Dictionary to store students who attended with a particular date
 attendees = {
 	
 }
@@ -13,6 +13,7 @@ attendees = {
 
 class Student(object):
 	current_id = 0
+	new_date = datetime.date.today()
 	def __init__(self, fname, lname, country = map_country["KE"]):
 		Student.current_id += 1
 		self.id = Student.current_id
@@ -20,6 +21,8 @@ class Student(object):
 		self.lname = lname
 		self.map_country = country
 
-	def attended(self, date = datetime.date.today):
+	#Method for collecting the information if a particular student attended
+	def attended(self, date = datetime.date.today()):
 		self.date = date
+		#Store a person along with the date he attended class
 		attendees[self.fname + " " + self.lname] = self.date
