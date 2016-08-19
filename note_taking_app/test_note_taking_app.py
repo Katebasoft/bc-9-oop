b961390b18a17219069e725784b7c1ecd37781c0
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-19 16:01:38 +0300

    Adds type verification in the get function

--- note_taking_app/test_note_taking_app.py
+++ note_taking_app/test_note_taking_app.py
@@ -5,15 +5,16 @@ from note_taking_app import note_taking_app
 class NoteTakingApp(unittest.TestCase):
 	
 	def setUp(self):
-			self.notes = ["Innocent", "Kateba"]
+		self.notes = ["Innocent", "Kateba"]
 	
 	def test_create_note_taking_app(self):
 		'''
-		Test for the super sum
+		Test for the create function
 		'''
 		self.assertEqual(create(""), None, "Unsupported data type")
 		self.assertEqual(create("Innocent"), "Innocent", "nothing was created")
 
+	def test_get(self):
 
 
 if __name__ == '__main__':
