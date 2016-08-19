79a0096ffbf4c184305854f05b75e29d0036757b
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-19 13:04:38 +0300

    Adds the setUp function

--- note_taking_app/test_note_taking_app.py
+++ note_taking_app/test_note_taking_app.py
@@ -3,18 +3,18 @@ import unittest
 from note_taking_app import note_taking_app
 
 class NoteTakingApp(unittest.TestCase):
-	'''
+	
 	def setUp(self):
-			self.numbers = [10,5,7,8,90,60]
-	'''
+			self.notes = ["Innocent", "Kateba"]
+	
 	def test_create_note_taking_app(self):
 		'''
 		Test for the super sum
 		'''
 		self.assertEqual(create(""), None, "Unsupported data type")
 		self.assertEqual(create("Innocent"), "Innocent", "nothing was created")
-		self.assertEqual(
-		self.assertEqual(super_sum(0,2,[-9,2,1]),-4, "wrong answer")
+
+
 
 if __name__ == '__main__':
 	unittest.main()
