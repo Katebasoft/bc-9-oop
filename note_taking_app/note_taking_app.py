12fba1760983cad4279b0c21a62739bce8d11795
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-19 17:23:14 +0300

    Adds a better approach for all methods and tests

--- note_taking_app/note_taking_app.py
+++ note_taking_app/note_taking_app.py
@@ -1,43 +1,54 @@
-'''
-A class for taking down notes for different authors
-'''
-
-class NotesApplication(object):
+class  NotesApplication(object):
 
 	notes_list = []
+
 	def __init__(self, author):
+
 		self.author = author
 		self.notes_list = NotesApplication.notes_list
 
-	def create(self, note_content):
-		if note_content != "":
-			self.notes_list.append(note_content) 
+	def create(self, note_content=None):
 
+		note_content = str(note_content)
+
+		if note_content != 'None':
+			self.notes_list.append(note_content)
+			return self.notes_list
+
+		else:
+			return "Empty content passed"
 
 	def list(self):
-		for writer_id in range(len(self.notes_list)):
-			print "Note ID: " + str(writer_id)
-			print self.notes_list[writer_id] 
-			print "By Author " + self.author
+		return self.notes_list
 
 	def get(self, note_id):
-		return NotesApplication.notes_list[note_id][0]
-
+		try:
+			return self.notes_list[note_id]
+		except:
+			return "No such content"
 
 	def search(self, search_text):
-		print "Showing results for search " + search_text
-		index = 0
-		for data in NotesApplication.notes_list:
-			content = data.values()[0]
-			author = data.keys()[0]
-			if content.find(search_text) != -1:
-				print "Note ID: " + str(index)
-				print content
-				print "By Author: " + author
-			index += 1
 
-	def delete(self, note_id):
-		self.notes_list.pop(note_id)
+		for text in self.notes_list:
+			if text.find(search_text) > -1:
+				return ('Showing results for %s \n Note ID [%s] \n %s \n By Author [%s]'
+				 %(search_text, self.notes_list.indexOf(text), text, self.author))
 
-	def edit(self, note_id, new_content):
-		self.notes_list[note_id] = new_content
+			return 'Content not Found'
+
+	def delete(self, notes_id):
+		try:
+			deleted_note = notes_list.pop([notes_id])
+			return deleted_note
+
+		except:
+			return "No such content"
+
+	def edit(self, notes_id, new_content):
+		try:
+			notes_list.insert(notes_id, new_content)
+			return notes_list
+		except:
+			return "Note to be edited doesn't exist"
+
+
--- note_taking_app/test_note_taking_app.py
+++ note_taking_app/test_note_taking_app.py
@@ -1,4 +1,57 @@
 import unittest
+from note_taking_app import NotesApplication
+
+'''
+	Note taking tests
+'''
+
+class NoteTakingTests(unittest.TestCase):
+	
+
+	def test_note_empty(self):
+		#Test for empty 
+		self.assertEqual(NotesApplication("Innocent Kateba is flying").create(), "Empty content passed")
+
+
+	def test_note_list(self):
+		#Test for the list function
+		inno = NotesApplication("Innocent")
+		notes_list = inno.create("Innocent Kateba is flying")
+		self.assertEqual(inno.list(), notes_list)
+
+	def test_note_get(self):
+		#Checking for existince
+		inno = NotesApplication("Innocent")
+		notes_list = inno.create("Are you there")
+		self.assertEqual(inno.get(8), "No such content")
+
+	def test_note_delete(self):
+		inno = NotesApplication("Innocent")
+		notes_list = inno.create("Innocent Kateba is flying")
+		self.assertEqual(inno.delete(4), "No such content")
+
+	def test_note_edit(self):
+		#content that doesn't exist can't be tested
+		inno = NotesApplication("Innocent")
+		notes_list = inno.create("Innocent Kateba is flying")
+		self.assertEqual(inno.edit(4, "Save"), "Note to be edited doesn't exist")
+
+	def test_note_search(self):
+		#search for returning no content found
+		inno = NotesApplication("Innocent")
+		notes_list = inno.create("Innocent Kateba is flying")
+		self.assertEqual(inno.search("Ruby"), "Content not Found")		
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
+
+'''
+import unittest
 
 from note_taking_app import note_taking_app
 
@@ -8,16 +61,17 @@ class NoteTakingApp(unittest.TestCase):
 		self.notes = ["Innocent", "Kateba"]
 	
 	def test_create_note_taking_app(self):
-		'''
-		Test for the create function
-		'''
+		
+		#Test for the create function
 		self.assertEqual(create(""), None, "Unsupported data type")
 		self.assertEqual(create("Innocent"), "Innocent", "nothing was created")
 
-	def test_get(self):
+	def test_search(self):
+		self.assertEqual(search("Innocent"), )
 
 
 if __name__ == '__main__':
 	unittest.main()
 
 
+'''
