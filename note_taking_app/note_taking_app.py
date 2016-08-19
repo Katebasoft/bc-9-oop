5ac37bae99b51a631068bc677300127894187ce5
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-19 12:38:37 +0300

    Adds immproved version of note_taking_app that lists all notes of the author

--- note_taking_app/note_taking_app.py
+++ note_taking_app/note_taking_app.py
@@ -7,22 +7,18 @@ class NotesApplication(object):
 	notes_list = []
 	def __init__(self, author):
 		self.author = author
+		self.notes_list = NotesApplication.notes_list
 
 	def create(self, note_content):
-		self.data = {self.author : note_content}
-		self.index = len(NotesApplication.notes_list) + 1
-		NotesApplication.notes_list.append(self.data)
+		if note_content != "":
+			self.notes_list.append(note_content) 
 
 
 	def list(self):
-		index = 0
-		for data in NotesApplication.notes_list:
-			index += 1
-			content = data.values()[0]
-			author = data.keys()[0]
-			print "Note ID: " + str(index)
-			print content
-			print "By Author " + author
+		for writer_id in range(len(self.notes_list)):
+			print "Note ID: " + str(writer_id)
+			print self.notes_list[writer_id] 
+			print "By Author " + self.author
 
 	def get(self, note_id):
 		return NotesApplication.notes_list[note_id][0]
@@ -41,10 +37,7 @@ class NotesApplication(object):
 			index += 1
 
 	def delete(self, note_id):
-		NotesApplication.notes_list.delete(note_id)
+		self.notes_list.pop(note_id)
 
 	def edit(self, note_id, new_content):
-		#data_for_the_id = NotesApplication.notes_list[note_id].values()[0]
-		#data_for_the_id = new_content
-		NotesApplication.notes_list[note_id].values()[0] = new_content
-		print "shift " + NotesApplication.notes_list[note_id].values()[0]
+		self.notes_list[note_id] = new_content
